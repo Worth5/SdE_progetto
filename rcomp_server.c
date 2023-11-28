16d997af1ac23b83f4daf4a44de32ce77a998603
Worth5 <38665988+Worth5@users.noreply.github.com>
2023-11-28 20:27:53 +0100

    Create rcomp_server.c

---
+++ rcomp_server.c
@@ -0,0 +1,16 @@
+
+#include <stdint.h>	//for uint8_t
+#include <stdio.h>	//for standard I/O
+#include <string.h>	//for strerror()
+#include <errno.h>	//for errno
+#include <fcntl.h>	//for flags like O_RDONLY and SEEK_END
+#include <stdlib.h>	//for exit() and EXIT_FAILURE
+#include <unistd.h>	//for unbuffered functions-> read(), wriqte(), STDIN_FILENO
+
+
+
+int main(int argc, char* argv){
+
+}
+
+
