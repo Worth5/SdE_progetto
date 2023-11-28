077dcb96cc6a61d7593f95c95147eb2cb5ef09db
Worth5 <38665988+Worth5@users.noreply.github.com>
2023-11-28 20:15:54 +0100

    Create  rcomp_client.c

---
+++ rcomp_client.c
@@ -0,0 +1,13 @@
+
+#include <stdint.h>	//for uint8_t
+#include <stdio.h>	//for standard I/O
+#include <string.h>	//for strerror()
+#include <errno.h>	//for errno
+#include <fcntl.h>	//for flags like O_RDONLY and SEEK_END
+#include <stdlib.h>	//for exit() and EXIT_FAILURE
+#include <unistd.h>	//for unbuffered functions-> read(), write(), STDIN_FILENO
+
+int main(int argc, char* argv){
+  
+}
+
