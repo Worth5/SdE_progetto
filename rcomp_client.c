32667c0c29e0b459f4bcbf975a51cf89b26a94da
Worth <38665988+Worth5@users.noreply.github.com>
2023-12-02 17:16:52 +0100

    Update rcomp_client.c
    bozza di client

--- rcomp_client.c
+++ rcomp_client.c
@@ -8,7 +8,24 @@
 #include <unistd.h>	//for unbuffered functions-> read(), write(), STDIN_FILENO
 #include <sys/socket.h> //for int socket(int domain, int type, int protocol);
 
+//idea creo una struttura che contiene comando e argomento
+/*
+struct request{
+    char* command
+    char* argument
+};
+*/
+request get_request();    //qui è dichiarata la funzione
+manage_request(request* myrequest);
+
 int main(int argc, char* argv){
-  
+    setup(address, port);   //gestisce la connessione
+                            //probabilmente la funzione deve restituire un socket?
+    do{
+        request = myrequest; //creo struttura request
+        myrequest = get_request();
+        manage_request(&request);   //probabilmente a manage request passato anche il socket
+    }
+    while(strcomp(command,"quit"));
 }
 
