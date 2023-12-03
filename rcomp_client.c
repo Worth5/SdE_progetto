458d66fcc04eee9231f4c53cb1669cc810dc42b5
Worth <38665988+Worth5@users.noreply.github.com>
2023-12-03 19:11:57 +0100

    Update rcomp_client.c

--- rcomp_client.c
+++ rcomp_client.c
@@ -1,31 +1,93 @@
 
-#include <stdint.h>	//for uint8_t
-#include <stdio.h>	//for standard I/O
-#include <string.h>	//for strerror()
-#include <errno.h>	//for errno
-#include <fcntl.h>	//for flags like O_RDONLY and SEEK_END
-#include <stdlib.h>	//for exit() and EXIT_FAILURE
-#include <unistd.h>	//for unbuffered functions-> read(), write(), STDIN_FILENO
-#include <sys/socket.h> //for int socket(int domain, int type, int protocol);
+#include <stdint.h>			//for uint8_t
+#include <stdio.h>			//for standard I/O
+#include <string.h>			//for strerror()
+#include <errno.h>			//for errno
+#include <fcntl.h>			//for flags like O_RDONLY and SEEK_END
+#include <stdlib.h>			//for exit() and EXIT_FAILURE
+#include <unistd.h>			//for unbuffered functions-> read(), write(), STDIN_FILENO
+#include <sys/socket.h> 	//for int socket(int domain, int type, int protocol);
+#include <arpa/inet.h>		//for uint16_t htons(uint16_t data);
 
-//idea creo una struttura che contiene comando e argomento
-/*
-struct request{
-    char* command
-    char* argument
+
+struct request{				//idea creo una struttura che contiene comando e argomento
+    char* command;
+    char* argument;
 };
-*/
-request get_request();    //qui è dichiarata la funzione
+int setup (int argc, char* argv[]);
+request get_request();                    
 manage_request(request* myrequest);
 
-int main(int argc, char* argv){
-    setup(address, port);   //gestisce la connessione
-                            //probabilmente la funzione deve restituire un socket?
+
+int main(int argc, char* argv[]){
+
+	request rq;				//struttura request
+	int sd;					//socket descriptor
+    sd=setup(argc, argv);   //gestisce la creazione socket e connessione al server
+	
     do{
-        request = myrequest; //creo struttura request
-        myrequest = get_request();
-        manage_request(&request);   //probabilmente a manage request passato anche il socket
+        rq = get_request(); //creo struttura request
+        manage_request(&rq,sd);
     }
-    while(strcomp(command,"quit"));
+    while(strcomp(rq.command,"quit"));
 }
 
+
+
+int setup(int argc, char* argv[]){
+
+    char* addr_str = "127.0.0.1";
+    int port_no = 1234;
+    in_addr_t address;
+
+    if(argc == 3){
+        if(strchr(argv[1],'.')!=NULL){
+            char* addr_str=argv[1];
+            port_no = atoi(argv[2]); //manca controllo errore.... meglio cmbiare funzione completamente
+        }
+        else {
+            char* addr_str=argv[2]
+            port_no = atoi(argv[1]); //manca controllo errore.... meglio cmbiare funzione completamente
+        }
+	}
+    else if(argc == 2){
+        if(strchr(argv[1],'.')!=NULL)
+			char* addr_str=argv[1];
+		else
+			port_no=atoi(argv[1]);
+	}
+	
+	in_addr_t address;
+    if (inet_pton(AF_INET, addr_str, (void*)&address) < 0){
+        fprintf(stderr, "Impossibile convertire l'indirizzo: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+
+    int sd;
+    if ( (sd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        fprintf(stderr, "Impossibile creare il socket: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    } 
+
+	// preparazione della struttura contenente indirizzo IP e porta
+    struct sockaddr_in sa;
+    sa.sin_family = AF_INET;
+    sa.sin_port = htons(port_no);
+    sa.sin_addr.s_addr = address;
+
+	//cnnessione al server
+	printf("Connessione...\n");
+    if (connect(sd, (struct sockaddr*)&sa, sizeof(struct sockaddr_in)) < 0){
+        fprintf(stderr, "Impossibile connettersi: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+	return sd;	//ritorno socket descriptor
+  }    
+
+
+request get_request(){
+	printf("rcomp> ");
+	
+}
